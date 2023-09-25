010ac7a0df0a5b43c8c5b26f2e72cf8742ed0f91
arthurshk <arthurdragon39@gmail.com>
2023-09-25 21:06:34 +0300

    commit

--- E Commerce Store/Controllers/HomeController.cs
+++ E Commerce Store/Controllers/HomeController.cs
@@ -30,22 +30,58 @@ namespace E_Commerce_Store.Controllers
             ViewData["Products"] = products;
             ViewData["Categories"] = categories;
             ViewData["NewProducts"] = products;
+            ViewData["Title"] = string.Format("Home | Shop");
+
             return View();
         }
         [HttpGet("/category/{id}/{url}")]
         public async Task<IActionResult> Category(int id)
         {
+            var search = HttpContext.Request.Query["search"].ToString();
             var category = await _siteContext.Categories
                 .Include(x => x.Image)
-                .Include(x => x.Products)
                 .FirstOrDefaultAsync(x => x.Id == id);
+            var products = await _siteContext.Products
+                .Include(x => x.MainImage)
+                .Where(x => x.Category.Id == category.Id)
+                .Where(x => x.Title.ToLower().Contains(search))
+                .ToListAsync();
+            ViewData["Search"] = search;
 
+            ViewData["Products"] = products;
+            
             ViewData["Categories"] = await _siteContext.Categories
                 .Include(x => x.Image)
                 .Include(x => x.Products)
                 .ThenInclude(x => x.MainImage)
                 .ToListAsync();
+
+            ViewData["Title"] = string.Format("{0} | Shop", category.Title);
+
             return View(category);
+        }
+        [HttpGet("/product/{id}")]
+        public async Task<IActionResult> Product(int id)
+        {
+            ViewData["Categories"] = await _siteContext.Categories
+             .Include(x => x.Image)
+             .Include(x => x.Products)
+             .ThenInclude(x => x.MainImage)
+             .ToListAsync();
+
+            var product = await _siteContext.Products
+                .Include(x => x.MainImage)
+                .FirstAsync(x => x.Id == id);
+
+            var otherProducts = await _siteContext.Products
+              .Include(x => x.MainImage)
+              .Where(x => x.Category.Id == product.Category.Id)
+              .Where(x=>x.Id != product.Id)
+              .ToListAsync();
+            ViewData["OtherProducts"] = otherProducts;
+            ViewData["Title"] = string.Format("{0} | Shop", product.Title);
+            return View(product);
+
         }
     }
 }
